3e9c21e7b9858038187eb019dce82471d6196f5e
Fraser <Fraser@FRASER-PC>
2016-12-01 23:28:17 -0500

    Add project files.

---
+++ EmployeeInformation/FrmInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInformation
+{
+    public partial class frmInput : Form
+    {
+        public frmInput()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Close form
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Clear text fields
+            txtEmployeeName.Clear();
+            txtEmployeeNum.Clear();
+            txtHoursWorked.Clear();
+
+            //Focus back to employee name text field
+            txtEmployeeName.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmOutput newOutputForm = new frmOutput();
+            newOutputForm.Show();
+
+            this.Hide();
+        }
+    }
+}
